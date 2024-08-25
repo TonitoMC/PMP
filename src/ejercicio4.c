724116c817986cd4106291040591196d3d1e9ffe
José Antonio Mérida <joseameridac@gmail.com>
2024-08-24 21:45:41 -0600

    Add Ejercicio4

---
+++ src/ejercicio4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    // Definicion de Variables
+    int variable1 = 0;
+    int variable2 = 0;
+    int n = 50;
+
+    // Inicializacion region paralela con variable1 compartida y variable2 privada
+    #pragma omp parallel for shared(variable1) private(variable2)
+    for (int i = 0; i < n; i++) {
+        // Modificacion de variables
+        variable1 += i;
+        variable2 += i;
+
+        // Mostramos los valores de las variables en cada iteración
+        printf("Thread %d, iter %d: variable1 = %d, variable2 = %d\n", omp_get_thread_num(), i, variable1, variable2);
+    }
+
+    printf("Final: variable1 = %d, variable2 = %d\n", variable1, variable2);
+
+    return 0;
+}
